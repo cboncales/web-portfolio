fefb57443ab687778fc8dff88139f47774f48639
cboncales <clarkboncales115@gmail.com>
2025-01-18 18:26:24 +0800

    first commit

---
+++ my-portfolio/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
